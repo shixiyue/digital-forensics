c67e7f0fcf50a3e8f0eefddc1be9f647d39a6424
shixiyue <shixiyue97@gmail.com>
2020-04-03 11:20:46 -0400

    login-signup

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, make_response, render_template
 from werkzeug.utils import secure_filename
 import os
 import script
+from forms import LoginForm, SignupForm
 
 import uuid
 
@@ -10,18 +11,42 @@ import uuid
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config['UPLOAD_FOLDER'] = "static/upload/"
-
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 # a route where we will display a welcome message via an HTML template
 @app.route("/")
 def index():
-    message = "Hello, World"
     return render_template('index.html')
 
 @app.route("/about")
 def about():
-    message = "Hello, World"
     return render_template('index.html')
+
+@app.route('/logout', methods = ['GET'])
+def logout():
+    resp = make_response(redirect(url_for('index')))
+    resp.delete_cookie('df_user')
+    return resp
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+      flash('Login requested for user {}, remember_me={}'.format(
+        form.email.data, form.remember_me.data))
+      resp = make_response(redirect(url_for('index')))
+      resp.set_cookie('df_user', form.email.data)
+      return resp
+    return render_template('login.html', form=form)
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+      flash('Verification email sent to {}'.format(form.email.data))
+      resp = make_response(redirect(url_for('index')))
+      return resp
+    return render_template('signup.html', form=form)
 
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
@@ -51,4 +76,5 @@ def upload():
 
 # run the application
 if __name__ == "__main__":
+    #app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
